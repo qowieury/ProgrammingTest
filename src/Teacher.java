b1ebb42a6ab73b7e6975d67352cef9589ac90d02
lalitaban <lalita.bandasak@gmail.com>
2016-11-15 14:27:50 +0700

    User,teacher,student[1]

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,10 @@
 public class Teacher extends User {
+    public Teacher(String name,String password){
+
+        this.type = USER_TEACHER;
+        this.name = name;
+        this.password = password;
+
+    }
+
 }
